c89f362ffb2dbf55987a1e6f9b2bab574c7b1812
jagadeeshram1119 <kjagadeeshram@gmail.com>
2025-03-10 18:22:33 +0530

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var typed = new Typed(".auto-type", {
-  Strings: ["developer", "designer", "programmer", "content creator"],
-  typeSpeed: 100,
-  backSpeed: 80,
-  loop: true,
-  backDelay: 1000,
-  showCursor: true,
-  cursorChar: "|",
-  smartBackspace: true,
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".auto-type", {
+    strings: ["developer", "designer", "programmer", "content creator"],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true,
+    backDelay: 1000,
+    showCursor: true,
+    cursorChar: "|",
+    smartBackspace: true,
+  });
+
+  console.log("connected");
 });
+
+console.log("connected");
